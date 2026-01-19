2d0865eb050e0f01ca1767762198d1eb888dc71c
rohitJung <rokshh28@gmail.com>
2026-01-19 11:49:59 +0545

    made guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+use rand::Rng;
+use std::{cmp::Ordering, io::{self, Write}};
+
+fn main() {
+    println!("Guessing Game!");
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    println!("Random number is {random_number}");
+
+    loop {
+        print!("Enter your guess: ");
+
+        // print doesn't flushes out the stdout like println as println has a `\n`
+        // if you want a immediate output flush the stdout yourself
+        io::stdout().flush().unwrap();
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error getting stdin");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
+
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Too little"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You guessed right");
+                break;
+            }
+        }
+    }
+}
