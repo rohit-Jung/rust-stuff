fa5f186bee9076e783c05ad6baa2b20a5546a684
rohitJung <rokshh28@gmail.com>
2026-01-20 23:31:53 +0545

    idk

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    let mut x = 5;
+    // let  x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    let spaces = "  "; // this is string
+    let spaces: usize = spaces.len(); // this is u32
+
+    println!("{}", -5 / 3);
+    let t = ([1; 2], [3; 4]);
+    let (a, b) = t;
+    println!("{}", t.0[0]);
 }
